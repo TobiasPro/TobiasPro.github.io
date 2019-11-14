0a3bc555f18cdcb34f989e79b6ff5bcaa82b34bd
Tobias <tobiasbjerregaard@live.dk>
2019-11-14 10:33:47 +0100

    stickman

---
+++ stickman/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(220);
+}
+function draw() {
+    background(225);
+	strokeWeight(10);
+	// head
+  ellipse(250, 70, 100, 100);
+
+  // hands
+  line(250, 150, 150, 250);
+  		line(250, 150, 350, 250);
+  	
+// Legs
+  line(250, 300, 350, 400);
+  	line(250, 300, 150, 400);
+  //body
+  	line(250, 120, 250, 300);
+  }
+  
